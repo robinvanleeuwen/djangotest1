64478fbf3bfae8e1ebcfa9e45a5b12a9019e4e08
Robin van Leeuwen <robinvanleeuwen@gmail.com>
2015-03-22 00:51:20 +0100

    Simple client-country v0.3

--- app/forms.py
+++ app/forms.py
@@ -14,7 +14,11 @@ class ClientForm(ModelForm):
     class Meta:
         model = Client
         exclude = [()]
-
+    name1 = forms.CharField(widget=forms.TextInput(attrs={"size":40}))
+    name2 = forms.CharField(widget=forms.TextInput(attrs={"size":40}))
+    street = forms.CharField(widget=forms.TextInput(attrs={"size":40}))
+    zip = forms.CharField(widget=forms.TextInput(attrs={"size":10}))
+    city = forms.CharField(widget=forms.TextInput(attrs={"size":28}))
     country = forms.ModelChoiceField(queryset=Country.objects.all(),
                                      initial=Country.objects.all().filter(country="NL"))
 
