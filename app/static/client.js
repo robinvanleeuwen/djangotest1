3620031f067e46eb1a77303049fcf0e9085217fd
Robin van Leeuwen <robinvanleeuwen@gmail.com>
2015-03-26 10:27:32 +0100

    Fix ajax multi-id autocomplete

--- app/static/client.js
+++ app/static/client.js
@@ -1,21 +1,19 @@
 $(document).ready(function() {
+    field_id = $(this).attr("id");
 
-    $("#id_client_number").autocomplete({
-        source: "/client/search/client_number",
+    $("#id_client_number, #id_name1").autocomplete({
+
+        source: function(request, callback){
+            $.getJSON("/client/search/"+this.element[0].id.substring(3), { term: request.term }, callback);
+        },
         minLength: 1,
         messages: {
             noResults: "",
             results: function() {}
         }
     });
-    $("#id_name1").autocomplete({
-       source: "/client/search/name1",
-        minLength: 1,
-        messages: {
-            noResults: "",
-            results: function() {}
-        }
-    });
+
+
     $("#id_client_number").focusout(function() {
         client_data = $.getJSON("/client/get/"+$("#id_client_number").val())
 
